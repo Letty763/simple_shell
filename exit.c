3d4a1f84802d7b947724e4cd4e3479f8abc175e4
SenamT <senny2513@gmail.com>
2023-05-26 18:04:53 -0700

    Simple_Shell part 1 Senam

---
+++ exit.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - this duplicates a string
+ *@dest: the target string to be duplicated
+ *@src: this is the source string
+ *@n: the number of characters to be duplicated
+ *Return: it should be a concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - this will concatenate two strings
+ *@dest: the initial string
+ *@src: the latest string
+ *@n: the number of bytes to be paramountly used
+ *Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - finds a character in a string
+ *@s: the string to be inspected
+ *@c: the character to search for
+ *Return:  a value to the memory area
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
